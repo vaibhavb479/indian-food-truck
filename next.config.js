4e1ab65e1dcec6b329d7338d5b7183db0a7787ab
vaibhavb479 <vaibhavb479@gmail.com>
2023-01-07 14:39:50 +0530

    config.js file changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  withVideos : require('next-videos'),
-  
 }
 
-export default nextConfig
+module.exports = nextConfig

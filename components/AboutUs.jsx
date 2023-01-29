1184241e74f83363255b1b25fe81dfe781f95c3e
vaibhavb479 <vaibhavb479@gmail.com>
2023-01-29 22:42:23 +0530

    about us page

--- components/AboutUs.jsx
+++ components/AboutUs.jsx
@@ -7,6 +7,8 @@ import "react-multi-carousel/lib/styles.css";
 import s6 from "../assets/s6.png";
 import s5 from "../assets/s5.png";
 import s4 from "../assets/s4.png";
+import mango from "../assets/New Folder/mango.jpg";
+import chole from "../assets/New Folder/chole.jpg";
 import foodTruck2 from "../assets/foodTruck2.png";
 import foodTruck4 from "../assets/foodTruck4.png";
 import foodTruck5 from "../assets/foodTruck5.png";
@@ -176,6 +178,66 @@ export default function AboutUs() {
         </div>
       </section>
     <Services/>
+    <section data-bs-version="5.1" className="features5 cid-sK8XwkDPAS" id="features5-7">
+    
+
+    
+    <div className="two_background"></div>
+    
+        <div className="row justify-content-center">
+            <div className="col-12 col-md-3 col-lg-4">
+                <div className="image_left">
+                <Image
+                src={mango}
+                alt=""
+                objectFit="cover"
+                layout="intrinsic"
+              />
+                </div>
+            </div>
+            <div className="col-12 col-md-6 col-lg-4">
+                <div className="main_header">
+                    <h2 className={css.mbr_section_title}>SPECIALS</h2>
+                    <div className="menu_widget">
+                        
+                    
+                        
+                    <div className="list_widget">
+                            <h3 className={css.mbr_section_subtitle}>Delicious Steak</h3>
+                            <p className={css.mbr_text}>(Lorem ipsum dolor sit amet, consectetur) </p>
+                        </div>
+                        {/* <div className="list_widget">
+                            <h3 className={css.mbr_section_subtitle}>La Pizza Pasta</h3>
+                            <p className={css.mbr_text}>(Lorem ipsum dolor sit amet, consectetur)</p>
+                        </div> */}
+                        <div className="list_widget">
+                            <h3 className={css.mbr_section_subtitle}>Sweet Desert</h3>
+                            <p className={css.mbr_text}>(Lorem ipsum dolor sit amet, consectetur)</p>
+                        </div>
+                        <div className="list_widget">
+                            <h3 className={css.mbr_section_subtitle}>Shot and Drink</h3>
+                            <p  className={css.mbr_text}>(Lorem ipsum dolor sit amet, consectetur)</p>
+                        </div>
+                        <div  className="list_widget"><p className={css.listBtn}><a href="index.html" className="text-primary">VIEW MENU</a></p>
+
+                        </div>
+
+                        </div>
+                </div>
+            </div>
+
+            <div className="col-12 col-md-3 col-lg-4">
+                <div className="image_right">
+                <Image
+                src={chole}
+                alt=""
+                objectFit="cover"
+                layout="intrinsic"
+              />
+                </div>
+            </div>
+        </div>
+</section>
     </div>
   );
 }
